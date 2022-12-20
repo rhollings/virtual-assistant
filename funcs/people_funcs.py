59708d11afcda74877ba11e893a8747f5f1def63
Rod <rhollingsworth1997@gmail.com>
2022-12-19 21:06:47 -0700

    datetime err

--- funcs/people_funcs.py
+++ funcs/people_funcs.py
@@ -16,9 +16,12 @@ def return_birthday(month, day, year):
     bd = datetime.date(year, month, day)
     today = datetime.date.today()
     res = bd - today
-    if res < 0: # timedelta err
-        res += 365
+    
     _str = 'Your birthday is in {} days'.format(res.days)
     return _str
 
-print(return_birthday(12, 5, 2022))
+#print(return_birthday(10, 5, 2022))
+
+#current_date = datetime.datetime(10, 5, 2022)
+
+#print(int(current_date.strftime("%d")))
